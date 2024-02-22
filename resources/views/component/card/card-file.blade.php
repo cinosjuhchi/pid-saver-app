bb6f7700ecff8cd59b7ef9d3a64521494f04166f
zidaneathallah <zidanathallahwinata@gmail.com>
2024-02-22 11:46:14 +0700

    asep

--- resources/views/component/card/card-file.blade.php
+++ resources/views/component/card/card-file.blade.php
@@ -27,21 +27,7 @@
             </div>
         </a>
 
-        <a href="#" id="lihat-detail" class="cursor-pointer group">
-            <div class="card-file bg-white rounded-md p-3 group-hover:bg-gray-200 transition-all">
-                <img src="{{ asset('resources/image/fotojdn.png') }}" alt="" class="rounded-md h-60 object-cover w-full">
-                <div class="title flex items-center justify-between mt-3">
-                    <div class="title">
-                        <h1 class="font-bold text-sm">Nama File</h1>
-                        <p class="text-xs text-gray-400">21 February 2024</p>
-                    </div>
-                    <button class="hover:bg-white rounded-full transition">
-                        <i class="bi bi-three-dots-vertical text-xl"></i>
-                    </button>
-                </div>
-            </div>
-        </a>
-
+       
     
 
 </div>
